0e9bb65129b8698df4fdc202a673a2bb4d17fbd5
SChaotyx <AgustinAngelCAB@gmail.com>
2023-03-17 03:14:15 -0300

    Fix crash Idk what I'm doing xD

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include "PauseLayerHook.h"
 #include "MoreOptionsLayerHook.h"
 
+bool MenuLayer_init(MenuLayer* self) {
+    if (!matdash::orig<&MenuLayer_init>(self)) return false;
+    MoreOptionsLayerHook::OptionCheckUpdate();
+    return true;
+}
+
 void mod_main(HMODULE) {
+    matdash::add_hook<&MenuLayer_init>(gd::base + 0x1907B0);
     PauseLayerHook::LoadHooks();
     MoreOptionsLayerHook::LoadHooks();
-    MoreOptionsLayerHook::OptionCheckUpdate();
 }
