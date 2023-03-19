858b2cbb42f5f9b5eea56d8c0e990457a550018f
SChaotyx <AgustinAngelCAB@gmail.com>
2023-03-19 08:24:27 -0300

    More buttons in the pause menu

--- src/PauseLayerHook.cpp
+++ src/PauseLayerHook.cpp
@@ -16,22 +16,37 @@ bool PauseLayerHook::PauseLayerInit() {
 
 	auto optBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png"), this, (SEL_MenuHandler)(&PauseLayerHook::optionsLayer));
 	optBtn->setScale(0.85f);
-	optBtn->setPosition(winSize.width - 50, winSize.height - 50);
+	optBtn->setPosition(winSize.width - 50, 50);
 
     auto infoBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"), this, (SEL_MenuHandler)(&PauseLayerHook::levelInfoLayer));
 	infoBtn->setScale(0.85f);
-    infoBtn->setPosition(winSize.width - 50, winSize.height - 105);
+    infoBtn->setPosition(winSize.width - 50, winSize.height - 50);
+
+	auto challBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_infoBtn_001.png"), this, (SEL_MenuHandler)(&PauseLayerHook::challengesLayer));
+	challBtn->setScale(0.85f);
+    challBtn->setPosition(winSize.width - 50, winSize.height - 160);
+
+	auto ldbrBtn = CCMenuItemSpriteExtra::create(CCSprite::createWithSpriteFrameName("GJ_levelLeaderboardBtn_001.png"), this, (SEL_MenuHandler)(&PauseLayerHook::levelLeaderboardLayer));
+	ldbrBtn->setScale(0.85f);
+    ldbrBtn->setPosition(winSize.width - 50, winSize.height - 105);
 
 	auto menu = CCMenu::create();
 	menu->setPosition(0,0);
 	menu->addChild(optBtn);
 	menu->addChild(infoBtn);
+	menu->addChild(challBtn);
+	menu->addChild(ldbrBtn);
 	this->addChild(menu);
 
     return true;
 }
 
+void PauseLayerHook::challengesLayer(cocos2d::CCObject* pSender){
+	ChallengesPage::create()->show();
+}
+
 void PauseLayerHook::optionsLayer(cocos2d::CCObject* pSender) {
+	//MoreOptionsLayer::create()->show();
 	auto layer = OptionsLayer::create();
 	cocos2d::CCDirector::sharedDirector()->getRunningScene()->addChild(layer, 100);
 	layer->showLayer(false);
@@ -45,5 +60,15 @@ FLAlertLayer* PauseLayerHook::CreateLevelInfoLayer(GJGameLevel* level) {
 
 void PauseLayerHook::levelInfoLayer(cocos2d::CCObject* pSender) {
 	PauseLayerHook::CreateLevelInfoLayer(PlayLayer::get()->m_level)->show();
+}
+
+FLAlertLayer* PauseLayerHook::CreateLevelLeaderboardLayer(GJGameLevel* level) {
+	return reinterpret_cast<FLAlertLayer*(__fastcall*)(GJGameLevel*, GJUserScore*)>(
+		base + 0x17c440
+	)(level, nullptr);
+}
+
+void PauseLayerHook::levelLeaderboardLayer(cocos2d::CCObject* pSender) {
+	PauseLayerHook::CreateLevelLeaderboardLayer(PlayLayer::get()->m_level)->show();
 }
     
--- src/PauseLayerHook.h
+++ src/PauseLayerHook.h
@@ -6,10 +6,16 @@ class PauseLayerHook : public PauseLayer {
 public:
     static void LoadHooks();
 	bool PauseLayerInit();
-	void practiceMusicBtn(CCObject*);
-	auto PracticeMusicBtnToggle(CCSprite* toggleOn, CCSprite* toggleOff);
+
     static FLAlertLayer* CreateLevelInfoLayer(GJGameLevel* level);
 	void levelInfoLayer(cocos2d::CCObject* pSender);
+
+    static FLAlertLayer* CreateLevelLeaderboardLayer(GJGameLevel* level);
+	void levelLeaderboardLayer(cocos2d::CCObject* pSender);
+
 	void optionsLayer(cocos2d::CCObject* pSender);
+
+	void challengesLayer(cocos2d::CCObject* pSender);
+
 	void extraOptLayer(cocos2d::CCObject* pSender);
 };
