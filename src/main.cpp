43d8c1add028e2177131e49d20d8447d344713a0
SChaotyx <AgustinAngelCAB@gmail.com>
2023-03-20 00:52:39 -0300

    Debug

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include "MoreOptionsLayerHook.h"
 #include "EndLevelLayerHook.h"
 #include "MenuLayerHook.h"
+#include <matdash/console.hpp>
 
 void mod_main(HMODULE) {
-
+    matdash::create_console();
+    
     PauseLayerHook::LoadHooks();
     MoreOptionsLayerHook::LoadHooks();
     EndLevelLayerHook::LoadHooks();
