25af8b19d9f7f03ad80e1bbe8e3d4a23e150deac
SChaotyx <AgustinAngelCAB@gmail.com>
2023-03-27 04:46:13 -0300

    key binds

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -1,12 +1,41 @@
 #include "Hooks.h"
 
 #include "GDMenuLayer.h"
-#include "GDMoreOptionsLayer.h"
+//#include "GDMoreOptionsLayer.h"
 #include "GDEditLevelLayer.h"
 #include "GDPauseLayer.h"
 #include "GDPlayLayer.h"
 #include "GDLevelCompleteLayer.h"
 #include "GDOptionsLayer.h"
+
+#include "SCToolsLayer.h"
+
+auto cocos(const char* symbol) {
+    static auto mod = GetModuleHandleA("libcocos2d.dll");
+    return GetProcAddress(mod, symbol);
+}
+
+void CCKeyboardDispatcher_dispatchKeyboardMSG(CCKeyboardDispatcher* self, int key, bool down) {
+    if (down) {
+        if(auto play_layer = gd::GameManager::sharedState()->getPlayLayer()){
+            if(play_layer->get()->m_level->m_eLevelType == 2 ){
+                if (key == 'A') {
+                    SCToolsLayer::applyLevelSpeed(1);
+                } else if (key == 'S') {
+                    SCToolsLayer::applyLevelSpeed(0.75);
+                } else if (key == 'D') {
+                    SCToolsLayer::applyLevelSpeed(0.5);
+                } else if (key == 'F') {
+                    SCToolsLayer::applyLevelSpeed(0.25);
+                }
+            }
+            if(key == 'R' && !play_layer->m_hasCompletedLevel && !play_layer->m_bIsPaused) {
+			    reinterpret_cast<void(__thiscall*)(PlayLayer*)>(base + 0x20BF00)(play_layer);
+            }
+        }
+    }
+    matdash::orig<&CCKeyboardDispatcher_dispatchKeyboardMSG>(self, key, down);
+}
 
 
 void Hooks::Load() {
@@ -16,7 +45,9 @@ void Hooks::Load() {
     GDPauseLayer::Hook();
     GDLevelCompleteLayer::Hook();
     GDEditLevelLayer::Hook();
-    //GDMoreOptionsLayer::Hook();
     GDOptionsLayer::Hook();
+
+    // from https://github.com/matcool/ReplayBot
+    matdash::add_hook<&CCKeyboardDispatcher_dispatchKeyboardMSG>(cocos("?dispatchKeyboardMSG@CCKeyboardDispatcher@cocos2d@@QAE_NW4enumKeyCodes@2@_N@Z"));
 
 }
